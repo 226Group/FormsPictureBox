251609122857a27a398b4c66542b0990faeac108
Булатников Андрей <av64119@gmail.com>
2025-04-01 14:38:46 +0300

    3 задания с шариками

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,6 +23,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -32,31 +35,33 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.Color.Black;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 325);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(12, 420);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(35, 414);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 30);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Порисуем!";
+            this.button1.Text = "ПОРИСУЕМ!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button2.Location = new System.Drawing.Point(138, 420);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(272, 414);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(120, 30);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Еще порисуем!";
+            this.button2.Text = "ЕЩЕ ПОРИСУЕМ!";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -70,11 +75,38 @@
             this.timer2.Interval = 500;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(485, 418);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ПОЛЕТАЕМ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 500;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(175, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Работы с шариками";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
@@ -84,6 +116,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -94,5 +127,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,45 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace FormsPictureBox
 {
     public partial class Form1 : Form
     {
-        //Инициализация переменных и ресурсов
+        private bool leftMove, upMove;
+        private int ball_x, ball_y;
+        private int rad = 50;
+        private Pen p = new Pen(Color.Lime);
+        private SolidBrush fon = new SolidBrush(Color.Black); // фон для анимации
+        private SolidBrush fig = new SolidBrush(Color.Aquamarine);
         private Graphics gr;
         private Random rand = new Random();
-        private SolidBrush fonBrush = new SolidBrush(Color.BlanchedAlmond);
+        private Timer animationTimer;
+
 
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true; // включаем двойную буферизацию
 
+            // инициализация таймера для анимации
+            animationTimer = new Timer();
+            animationTimer.Interval = 50; // интервал обновления
+            animationTimer.Tick += Timer3_Tick;
+
+            ball_x = pictureBox1.Width / 2;
+            ball_y = pictureBox1.Height / 2;
+        }
+
+        // кнопка "Полетаем" для запуска анимации шарика
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // остановка других таймеров
+            timer1.Stop();
+            timer2.Stop();
+
+            gr = pictureBox1.CreateGraphics();
+            leftMove = true;
+            upMove = true;
+            fig = new SolidBrush(RandomColor());
+            // запуск анимации
+            animationTimer.Start();
+        }
+
+        // таймер для анимации шарика
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            // очистка предыдущего положения
+            gr.Clear(Color.Black);
+
+            // обновление координат
+            if (leftMove)
+            {
+                ball_x += 10;
+            }
+            else
+            {
+                ball_x -= 10;
+            }
+
+            if (upMove)
+            {
+                ball_y += 10;
+            }
+            else
+            {
+                ball_y -= 10;
+            }
+
+            // проверка столкновений с границами
+            if (ball_x <= rad) leftMove = true;
+            if (ball_x >= pictureBox1.Width - rad) leftMove = false;
+            if (ball_y <= rad) upMove = true;
+            if (ball_y >= pictureBox1.Height - rad) upMove = false;
+
+            // отрисовка шарика
+            gr.FillEllipse(fig, ball_x - rad, ball_y - rad, 2 * rad, 2 * rad);
+            gr.DrawEllipse(p, ball_x - rad, ball_y - rad, 2 * rad, 2 * rad);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //gr.Clear(fonBrush.Color);
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            timer2.Stop();
             gr = pictureBox1.CreateGraphics();
             timer1.Start();
+            timer2.Stop();
+            animationTimer.Stop();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            gr.Clear(fonBrush.Color);
+            //черный фон
+            gr.Clear(Color.Black);
 
             for (int i = 0; i < 15; i++)
             {
@@ -48,7 +107,7 @@ namespace FormsPictureBox
                 int y = rand.Next(pictureBox1.Height - 2 * rad);
 
                 using (var brush = new SolidBrush(RandomColor()))
-                using (var pen = new Pen(fonBrush.Color)) //new Pen(RandomColor()))
+                using (var pen = new Pen(Color.White)) // цвет обводки
                 {
                     gr.FillEllipse(brush, x, y, 2 * rad, 2 * rad);
                     gr.DrawEllipse(pen, x, y, 2 * rad, 2 * rad);
@@ -66,24 +125,24 @@ namespace FormsPictureBox
             timer1.Stop();
             gr = pictureBox1.CreateGraphics();
             timer2.Start();
+            animationTimer.Stop();
 
-            gr.Clear(fonBrush.Color);
+            // черный фон
+            gr.Clear(Color.Black);
         }
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-
             int rad = rand.Next(10, 50);
             int x = rand.Next(pictureBox1.Width - 2 * rad);
             int y = rand.Next(pictureBox1.Height - 2 * rad);
 
             using (var brush = new SolidBrush(RandomColor()))
-            using (var pen = new Pen(RandomColor()))
+            using (var pen = new Pen(Color.White)) // цвет обводки
             {
                 gr.FillEllipse(brush, x, y, 2 * rad, 2 * rad);
                 gr.DrawEllipse(pen, x, y, 2 * rad, 2 * rad);
             }
         }
-
-    }   
+    }
 }
